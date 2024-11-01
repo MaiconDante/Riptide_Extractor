a7ab6783329d450ea7fe199e2139168a34f18477
Maicondante <maicondante@hotmail.com>
2024-11-01 11:23:32 -0300

    Importando e instalando bibliotecas do projeto

---
+++ Main.py
@@ -0,0 +1,7 @@
+# Importando bibliotecas do projeto
+import customtkinter as ctk
+from tkinter import PhotoImage, filedialog
+from time import strftime
+from pytubefix import YouTube
+from threading import Thread
+import os
