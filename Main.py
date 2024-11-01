d676c2a08c68f7930fafe076d052b577dc801203
Maicondante <maicondante@hotmail.com>
2024-11-01 11:30:52 -0300

    Criando a Classe do aplicativo e exibindo a janela

--- Main.py
+++ Main.py
@@ -5,3 +5,14 @@ from time import strftime
 from pytubefix import YouTube
 from threading import Thread
 import os
+
+# Variável root recebendo comando CTk que faz aparecer a janela
+root = ctk.CTk()
+
+# Criando a Classe da aplicação onde receberá todas execuções
+class Application():
+    def __init__(self):  # Construtor que recebe as propriedades do aplicativo
+        self.root = root
+        root.mainloop()
+
+Application() 
